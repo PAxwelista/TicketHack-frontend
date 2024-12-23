1c169fb2074cb65fca812dda4decf7ec2fac7fc0
madotto.Axel@outlook.com <madotto.axel@outlook.com>
2024-12-23 19:00:29 +0100

    deploy

--- book.js
+++ book.js
@@ -1,38 +1,36 @@
-//------------------------------------------------------------------
-
-const routeStart = "https://tickethack-backend-three-beta.vercel.app/"
-fetch(`${routeStart}panier`)//recuperation des voyages dans la database panier
-    .then(response => response.json())
-    .then(data => {
-        if (data.alltrips.some(e=>e.isPaid)) {
-            document.querySelector("#cart").innerHTML = `
+const routeStart = "https://tickethack-backend-three-beta.vercel.app/";
+fetch(`${routeStart}panier`)
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.alltrips.some((e) => e.isPaid)) {
+      document.querySelector("#cart").innerHTML = `
             <span id = topText>My bookings</span>
             <div id="trajets">
             </div>
             <div id = separationLine></div>
             <span id = "bottomText">Enjoy your travels with Tickethack!</span>
-            `
-            for (const trip of data.alltrips) {  //ajout des blocs = ajout du innerHTML
-                
-                if (trip.isPaid){
-                    momentUntilDepart = moment(trip.date).fromNow();
-                    const correctedDate = new Date(trip.date);
-                    let correctedHour = correctedDate.getHours();
-                    if (Number(correctedHour)<10){correctedHour="0"+correctedHour};
-                    let correctedMinutes = correctedDate.getMinutes();
-                    if (correctedMinutes<10){correctedMinutes="0"+correctedMinutes};
-                    const newTrip = `<div class="allerretourContainer">
+            `;
+      for (const trip of data.alltrips) {
+        if (trip.isPaid) {
+          momentUntilDepart = moment(trip.date).fromNow();
+          const correctedDate = new Date(trip.date);
+          let correctedHour = correctedDate.getHours();
+          if (Number(correctedHour) < 10) {
+            correctedHour = "0" + correctedHour;
+          }
+          let correctedMinutes = correctedDate.getMinutes();
+          if (correctedMinutes < 10) {
+            correctedMinutes = "0" + correctedMinutes;
+          }
+          const newTrip = `<div class="allerretourContainer">
                         <span id="villes">${trip.departure} > ${trip.arrival}</span>
                         <span id="heure">${correctedHour}:${correctedMinutes}</span>
                         <span id="prix">${trip.price}€</span>
                         <span>Departure ${momentUntilDepart}</span>
                     </div>   `;
 
-                document.querySelector("#trajets").innerHTML += newTrip;
-                }
-            }//fin de la boucle
-            
-            }
-    })//fin du .then
-
-//---------------------------------------------------------------------------------------------------------
+          document.querySelector("#trajets").innerHTML += newTrip;
+        }
+      }
+    }
+  });
--- panier.js
+++ panier.js
@@ -1,85 +1,72 @@
-const routeStart= 'https://tickethack-backend-three-beta.vercel.app/'
-
-
-
-//***FONCTION UPDATECOUNTER***-----------------
+const routeStart = "https://tickethack-backend-three-beta.vercel.app/";
 
 function updateCounter() {
-
-    fetch(`${routeStart}panier/unpaid`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.unpaidTrips);
-            let allTrips = data.unpaidTrips;
-            let priceCounter = 0;
-            for (let trip of allTrips) { priceCounter += trip.price }
-            //console.log(priceCounter);
-            document.querySelector('#prixtotal').textContent = `total: ${priceCounter}€`;;
-
-        })
+  fetch(`${routeStart}panier/unpaid`)
+    .then((response) => response.json())
+    .then((data) => {
+      let allTrips = data.unpaidTrips;
+      let priceCounter = 0;
+      for (let trip of allTrips) {
+        priceCounter += trip.price;
+      }
+      if (allTrips.length) {
+        document.querySelector(
+          "#prixtotal"
+        ).textContent = `total: ${priceCounter}€`;
+      }
+    });
 }
-//***FONCTION ACTIVATEDELETEBUTTONS***-----------------
-
 function activateDeleteButtons() {
+  let deletes = document.querySelectorAll(".buttondelete");
 
-    let deletes = document.querySelectorAll('.buttondelete');//selection de tous les boutons delete
+  for (let i = 0; i < deletes.length; i++) {
+    deletes[i].addEventListener("click", function () {
+      const userId = deletes[i].value;
+      fetch(`${routeStart}panier`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ _id: userId }),
+      })
+        .then((response) => response.json())
+        .then(() => {
+          updateCounter();
 
-    for (let i = 0; i < deletes.length; i++) {
-        deletes[i].addEventListener("click", function () { //ajout de l'écoute
-
-            const userId = deletes[i].value;
-            fetch(`${routeStart}panier`, //clic: suprimer div-id de la database
-                {
-                    method: "DELETE",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({ _id: userId })
-                })
-                .then(response => response.json())
-                .then(() => {
-                    console.log("trip removed from cart");
-                    updateCounter();//clic: mettre à jour le compteur
-                    console.log(this.parentNode.parentNode.parentNode.children.length);
-                    if (this.parentNode.parentNode.parentNode.children.length>1){this.parentNode.parentNode.remove();}//clic: retirer l'élément parent   
-                    else {this.parentNode.parentNode.remove();
-                        displayEmptyCart();
-                    }
-
-                });
-
-
-        })
-    }//fin de la boucle
+          if (this.parentNode.parentNode.parentNode.children.length > 1) {
+            this.parentNode.parentNode.remove();
+          } else {
+            this.parentNode.parentNode.remove();
+            displayEmptyCart();
+          }
+        });
+    });
+  }
 }
-//-------------FONCTION AFFICHAGE PANIER VIDE-------------------------
 
-function displayEmptyCart(){document.querySelector("#cart").innerHTML=
-    
-    `<div class="emptyBasket">
+function displayEmptyCart() {
+  document.querySelector("#cart").innerHTML = `<div class="emptyBasket">
         <span class ="noBooking">No booking yet.</span>
         <span class ="noBooking">Why not plan a trip?</span>
 
-    </div>`}
+    </div>`;
+}
 
-//-------------MISE A JOUR DU PANIER A L'AFFICHAGE---------------------
-
-fetch(`${routeStart}panier`)//recuperation des voyages dans la database panier
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-
-
-        if (data.alltrips.some(e=>!e.isPaid))  {
-            let cumulatedPrice = 0;
-            for (const trip of data.alltrips) {  //ajout des blocs = ajout du innerHTML
-
-                if (!trip.isPaid) {
-
-                    const correctedDate = new Date(trip.date);
-                    let correctedHour = correctedDate.getHours();
-                    if (Number(correctedHour) < 10) { correctedHour = "0" + correctedHour };
-                    let correctedMinutes = correctedDate.getMinutes();
-                    if (correctedMinutes < 10) { correctedMinutes = "0" + correctedMinutes };
-                    const newTrip = `<div class="allerretourContainer">
+fetch(`${routeStart}panier`)
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.alltrips.some((e) => !e.isPaid)) {
+      let cumulatedPrice = 0;
+      for (const trip of data.alltrips) {
+        if (!trip.isPaid) {
+          const correctedDate = new Date(trip.date);
+          let correctedHour = correctedDate.getHours();
+          if (Number(correctedHour) < 10) {
+            correctedHour = "0" + correctedHour;
+          }
+          let correctedMinutes = correctedDate.getMinutes();
+          if (correctedMinutes < 10) {
+            correctedMinutes = "0" + correctedMinutes;
+          }
+          const newTrip = `<div class="allerretourContainer">
                         <span>${trip.departure} > ${trip.arrival}</span>
                         <span>${correctedHour}:${correctedMinutes}</span>
                         <span>${trip.price}€</span>
@@ -88,36 +75,29 @@ fetch(`${routeStart}panier`)//recuperation des voyages dans la database panier
                         </div>
                     </div>   `;
 
-                    document.querySelector("#trajets").innerHTML += newTrip;
-                    cumulatedPrice += trip.price
-                }
-            }//fin de la boucle
+          document.querySelector("#trajets").innerHTML += newTrip;
+          cumulatedPrice += trip.price;
+        }
+      }
 
-            document.querySelector('#prixtotal').textContent = `Total: ${cumulatedPrice}€`;//affichage du total initial
-            activateDeleteButtons()//activer boutons delete
-
-        }//fin du if(data.alltrips)
-else {
-    displayEmptyCart();
-};
-    
-    })//fin du .then
-
-//---------------------------------------------------------------------------------------------------------
-
+      document.querySelector(
+        "#prixtotal"
+      ).textContent = `Total: ${cumulatedPrice}€`;
+      activateDeleteButtons();
+    } else {
+      displayEmptyCart();
+    }
+  });
 function purchase() {
-    fetch(`${routeStart}panier/book`,
-        {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },//updater à isPaid = true les trips du panier
-        })
-        .then(response => response.json())
-        .then(() => {
-            console.log("trips paid");
-            document.querySelector('#prixtotal').textContent = `total: 0€`//mettre à 0 le compteur
-            window.location.assign("book.html")//renvoyer vers page 3
-        });
-};
-
+  fetch(`${routeStart}panier/book`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then(() => {
+      document.querySelector("#prixtotal").textContent = `total: 0€`;
+      window.location.assign("book.html");
+    });
+}
 
 document.querySelector("#button").addEventListener("click", purchase);
